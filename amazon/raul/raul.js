0700bb03d6531fb65b348aac54426330ebedce2e
Joriko <136170771+toinkan@users.noreply.github.com>
2023-11-08 10:13:37 -0300

    final

---
+++ amazon/raul/raul.js
@@ -0,0 +1,9 @@
+function img01 (){
+    document.getElementById("trocarimg").src="raul_frenteG.jpg";
+}
+function img02 (){
+    document.getElementById("trocarimg").src="raul_lado1G.jpg";
+}
+function img03 (){
+    document.getElementById("trocarimg").src="raul_lado2G.jpg";
+}
